b3ec7c045bee76646beefdbc0c6fddf3c5456e03
morghan marco santiago acuña <anita.pokorasky@gmail.com>
2016-08-20 17:13:52 -0300

    todo

---
+++ php/agregartip.php
@@ -0,0 +1,11 @@
+<?PHP
+	$con = mysqli_connect('localhost','admin','1234','db_tips');
+	
+	$country = "test";
+	$category = "test";
+	$likes = "test";
+	$dislikes = "test";
+	$message = "Hola bla bla bla";
+	$sql = "INSERT INTO tips (pais,categoria,likes,dislikes,message) VALUES ('$country','$category','$likes','$dislikes','$message')";
+	$con->query($sql);
+?>
---
+++ php/escribirtip.php
@@ -0,0 +1,15 @@
+<?PHP 
+	
+	$con = mysqli_connect('localhost','admin','1234','db_tips');
+	$sql = "SELECT * FROM tips ORDER BY likes DESC";
+	
+	if ($resultado = $con->query($sql)) {
+	    
+	    while ($fila = $resultado->fetch_assoc()) {
+	        echo "tip: " . $fila["message"] . " - ";
+	    }
+	    $resultado->free();
+	} else {
+		echo " No ";
+	}
+?>
